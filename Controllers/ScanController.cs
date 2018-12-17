5e3eab27db639775d26c5a1423a7b75065b8f2e5
Talen Fisher <tqfisher@iastate.edu>
2018-12-17 14:49:03 -0600

    remove creationDate parameter from scan put, post requests

--- Controllers/ScanController.cs
+++ Controllers/ScanController.cs
@@ -32,18 +32,28 @@ namespace api.Controllers{
         }
 
         [HttpPost]
-        public String Post(int authorId, int setId, int instrumentId, int barrelNo, int bulletNo, DateTime creationDate, int magnification, int threshold, int resolution) {
+        public String Post(int authorId, int setId, int instrumentId, int barrelNo, int bulletNo, int magnification, int threshold, int resolution) {
             using(var db = new TopazdbContext()){
-                db.Scans.Add(new Scan(){ authorId = authorId, setId = setId, instrumentId = instrumentId, barrelNo = barrelNo, bulletNo = bulletNo, creationDate = creationDate, magnification = magnification, threshold = threshold, resolution = resolution }); 
+                db.Scans.Add(new Scan() { 
+                    authorId = authorId, 
+                    setId = setId, 
+                    instrumentId = instrumentId, 
+                    barrelNo = barrelNo, 
+                    bulletNo = bulletNo, 
+                    magnification = magnification, 
+                    threshold = threshold, 
+                    resolution = resolution 
+                }); 
+                
                 db.SaveChanges(); 
             }
             return "Value Added Successfully"; 
         }
 
         [HttpPut("{id}")]
-        public string Put(int id, int authorId, int setId, int instrumentId, int barrelNo, int bulletNo, DateTime creationDate, int magnification, int threshold, int resolution) {
+        public string Put(int id, int authorId, int setId, int instrumentId, int barrelNo, int bulletNo, int magnification, int threshold, int resolution) {
             Delete(id); 
-            Post(authorId, setId, instrumentId, barrelNo, bulletNo, creationDate, magnification, threshold, resolution); 
+            Post(authorId, setId, instrumentId, barrelNo, bulletNo, magnification, threshold, resolution); 
             return "Value updated Successfully"; 
         }
 
